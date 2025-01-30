09eb1cb807cc343ba02892839ab06721b5cecc73
Alberto <albertogilsanjuan@gmail.com>
2025-01-30 19:14:56 +0100

    FIX: Cypress Login Test

--- cypress/e2e/authentication.cy.js
+++ cypress/e2e/authentication.cy.js
@@ -10,6 +10,18 @@ context('Authentication Test', () => {
     cy.get('[data-cy=emailInput]').should('be.visible')
   })
 
+  it('Test Navigation After Login', () => {
+    cy.get('[data-cy=emailInput]').type('a@gmail.com')
+    cy.get('[data-cy=passwordInput]').type('1234')
+    cy.get('[data-cy=submitButton]').click()
+
+    cy.get('[data-cy=dashboard]').should('be.visible')
+
+    cy.visit('http://localhost:5173/booking')
+
+    cy.get('[data-cy=bookings]').should('be.visible')
+  })
+
   it('Failed Login Test', () => {
     cy.get('[data-cy=emailInput]').type('b@gmail.com')
     cy.get('[data-cy=passwordInput]').type('0000')
--- src/bookings/pages/Bookings.jsx
+++ src/bookings/pages/Bookings.jsx
@@ -61,7 +61,7 @@ export const Booking = () => {
     )
 
     return (
-        <BookingStyled>
+        <BookingStyled data-cy='bookings'>
             <BookingMenuStyled>
                 <BookingMenuTextStyled>
                     <BookingMenuItemStyled>All Bookings</BookingMenuItemStyled>
